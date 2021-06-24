aa6026263847b5c8754f58bfc94a461cb2ce44ce
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-24 14:41:51 -0300

    Resolution Exercise 3 - part 2

---
+++ src/aula3/pratica2/ex3/Produto.java
@@ -0,0 +1,42 @@
+package aula3.pratica2.ex3;
+
+public class Produto {
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public Produto(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\nR$" + price + "\nQuantidade: " + quantity;
+    }
+}
---
+++ src/aula3/pratica2/ex3/Program.java
@@ -0,0 +1,41 @@
+package aula3.pratica2.ex3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Produto> produtos = new ArrayList<>();
+
+        for(int i = 1; i <= 3; i++) {
+            System.out.println("Produto " + i);
+            String name = sc.nextLine();
+
+            double price = sc.nextDouble();
+            sc.nextLine();
+
+            int quantity = sc.nextInt();
+            sc.nextLine();
+
+            produtos.add(new Produto(name, price, quantity));
+        }
+
+        double total = 0;
+
+        for(Produto prod : produtos) {
+            total += prod.getPrice() * prod.getQuantity();
+
+            System.out.println(prod);
+            System.out.println();
+        }
+
+        System.out.println("Valor Total: R$ " + String.format("%.2f", total));
+
+        sc.close();
+    }
+}
