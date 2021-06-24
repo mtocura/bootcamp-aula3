876cca4ab1999fff8549bbaba7a91c8548286cb0
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-24 14:13:37 -0300

    Resolution Exercise 1 - part 2

---
+++ src/aula3/pratica2/Exercicio1.java
@@ -0,0 +1,52 @@
+package aula3.pratica2;
+
+public class Exercicio1 {
+    /*
+    Dado um vetor de 10 posições, sendo respectivamente ocupado pelos valores 52, 10, 85, 1324, 01, 13, 62, 30, 12 e 127.
+    Imprima no console:
+    a) Sua ordenação crescente.
+    b) Sua ordenação decrescente.
+     */
+
+    public static void main(String[] args) {
+        int[] array = {52, 10, 85, 1324, 01, 13, 62, 30, 12, 127};
+
+        int[] crescente = bubbleSort(array, 'c');
+        System.out.println("Ordenação crescente:");
+        for(int i : crescente) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        int[] decrescente = bubbleSort(array, 'd');
+        System.out.println("Ordenação decrescente:");
+        for(int i : decrescente) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] bubbleSort(int[] arr, char tipo) {
+        int[] result = arr;
+
+        for(int i = arr.length - 1; i >= 1; i--) {
+            for(int j = 1; j <= i; j++) {
+                if(tipo == 'c') {
+                    if(arr[j-1] > arr[j]) {
+                        int aux = arr[j-1];
+                        arr[j-1] = arr[j];
+                        arr[j] = aux;
+                    }
+                } else if(tipo == 'd') {
+                    if(arr[j-1] < arr[j]) {
+                        int aux = arr[j-1];
+                        arr[j-1] = arr[j];
+                        arr[j] = aux;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
