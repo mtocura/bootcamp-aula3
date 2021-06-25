444b7b701f8ee3da8d5025312ad97be7719c495e
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-25 12:55:43 -0300

    Resolution Exercise 2

---
+++ src/aula4/ex2/Counter.java
@@ -0,0 +1,50 @@
+package aula4.ex2;
+
+public class Counter {
+    /*
+    Crie uma classe Counter (sim, contador de programação) com métodos que permitam aumentar e diminuir seu valor.
+    A classe conterá um construtor padrão, um construtor com parâmetros, um construtor de cópia e os setters e getters
+    correspondentes (métodos de acesso).
+     */
+
+    private Long counter;
+
+    public Counter(Long counter) {
+        this.counter = counter;
+    }
+
+    public Counter(Counter c) {
+        this.counter = c.getCounter();
+    }
+
+    public Long getCounter() {
+        return counter;
+    }
+
+    public void setCounter(Long counter) {
+        this.counter = counter;
+    }
+
+    public void add() {
+        this.counter++;
+    }
+
+    public void add(Long value) {
+        this.counter += value;
+    }
+
+    public void subtract() {
+        this.counter--;
+    }
+
+    public void subtract(Long value) {
+        this.counter -= value;
+    }
+
+    @Override
+    public String toString() {
+        return "Counter{" +
+                "counter=" + counter +
+                '}';
+    }
+}
---
+++ src/aula4/ex2/Program.java
@@ -0,0 +1,30 @@
+package aula4.ex2;
+
+public class Program {
+    public static void main(String[] args) {
+        Counter c1 = new Counter(0l);
+        Counter c2 = new Counter(10l);
+        Counter c3 = new Counter(c1);
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+        System.out.println();
+        System.out.println("===================");
+        c1.add();
+        System.out.println(c1);
+
+        c2.add(10l);
+        System.out.println(c2);
+
+        System.out.println();
+        System.out.println("===================");
+        c3.subtract();
+        System.out.println(c3);
+        c3.add(9l);
+        System.out.println(c3);
+        c3.subtract(9l);
+        System.out.println(c3);
+    }
+}
