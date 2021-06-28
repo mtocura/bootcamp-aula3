a0f7f5de34f855d2093a4509f82b5ce7f7365c29
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-28 17:51:02 -0300

    Refatocr Gerente to implements CLT

--- src/aula4/tt/Gerente.java
+++ src/aula4/tt/Gerente.java
@@ -2,8 +2,66 @@ package aula4.tt;
 
 import java.math.BigDecimal;
 
-public class Gerente extends CLT{
+public class Gerente implements CLT{
+    private BigDecimal salarioBase;
+    private Double horasSemanais;
+    private Double fimDeSemana;
+    private Double bonificacao;
+    private Integer metaAtingida;
+
     public Gerente(BigDecimal salarioBase, Double horasSemanais, Double fimDeSemana, Double bonificacao, Integer metaAtingida) {
-        super(salarioBase, horasSemanais, fimDeSemana, bonificacao, metaAtingida);
+        this.salarioBase = salarioBase;
+        this.horasSemanais = horasSemanais;
+        this.fimDeSemana = fimDeSemana;
+        this.bonificacao = bonificacao;
+        this.metaAtingida = metaAtingida;
+    }
+
+    public BigDecimal getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(BigDecimal salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public Double getHorasSemanais() {
+        return horasSemanais;
+    }
+
+    public void setHorasSemanais(Double horasSemanais) {
+        this.horasSemanais = horasSemanais;
+    }
+
+    public Double getFimDeSemana() {
+        return fimDeSemana;
+    }
+
+    public void setFimDeSemana(Double fimDeSemana) {
+        this.fimDeSemana = fimDeSemana;
+    }
+
+    public Double getBonificacao() {
+        return bonificacao;
+    }
+
+    public void setBonificacao(Double bonificacao) {
+        this.bonificacao = bonificacao;
+    }
+
+    public Integer getMetaAtingida() {
+        return metaAtingida;
+    }
+
+    public void setMetaAtingida(Integer metaAtingida) {
+        this.metaAtingida = metaAtingida;
+    }
+
+    @Override
+    public void pagarSalario() {
+        double valor = this.salarioBase.doubleValue() + (this.metaAtingida * this.bonificacao * this.salarioBase.doubleValue());
+        BigDecimal total = BigDecimal.valueOf(valor);
+
+        System.out.println("Salário base Gerente: R$" + String.format("%.2f", this.salarioBase.doubleValue()) + "\nValor a receber: R$ " + String.format("%.2f", total.doubleValue()));
     }
 }
