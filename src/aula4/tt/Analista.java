38f584f790676569cc8b91fb79eda10ac39e89f1
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-28 17:38:25 -0300

    Analista class extending CLT

---
+++ src/aula4/tt/Analista.java
@@ -0,0 +1,9 @@
+package aula4.tt;
+
+import java.math.BigDecimal;
+
+public class Analista extends CLT{
+    public Analista(BigDecimal salarioBase, Double horasSemanais, Double fimDeSemana, Double bonificacao, Integer metaAtingida) {
+        super(salarioBase, horasSemanais, fimDeSemana, bonificacao, metaAtingida);
+    }
+}
