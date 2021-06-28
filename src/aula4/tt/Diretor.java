f1c98443378f6b08e4e73a67fdf45f2e70c4aa13
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-28 17:39:15 -0300

    Diretor class extending CLT

---
+++ src/aula4/tt/Diretor.java
@@ -0,0 +1,9 @@
+package aula4.tt;
+
+import java.math.BigDecimal;
+
+public class Diretor extends CLT{
+    public Diretor(BigDecimal salarioBase, Double horasSemanais, Double fimDeSemana, Double bonificacao, Integer metaAtingida) {
+        super(salarioBase, horasSemanais, fimDeSemana, bonificacao, metaAtingida);
+    }
+}
