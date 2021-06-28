5353375e546cf51f1c10bb3133beee49ac609b5f
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-28 17:50:06 -0300

    Refactor CLT to interface

--- src/aula4/tt/CLT.java
+++ src/aula4/tt/CLT.java
@@ -2,65 +2,6 @@ package aula4.tt;
 
 import java.math.BigDecimal;
 
-public abstract class CLT {
-    private BigDecimal salarioBase;
-    private Double horasSemanais;
-    private Double fimDeSemana;
-    private Double bonificacao;
-    private Integer metaAtingida;
-
-    public CLT(BigDecimal salarioBase, Double horasSemanais, Double fimDeSemana, Double bonificacao, Integer metaAtingida) {
-        this.salarioBase = salarioBase;
-        this.horasSemanais = horasSemanais;
-        this.fimDeSemana = fimDeSemana;
-        this.bonificacao = bonificacao;
-        this.metaAtingida = metaAtingida;
-    }
-
-    public BigDecimal getSalarioBase() {
-        return salarioBase;
-    }
-
-    public void setSalarioBase(BigDecimal salarioBase) {
-        this.salarioBase = salarioBase;
-    }
-
-    public Double getHorasSemanais() {
-        return horasSemanais;
-    }
-
-    public void setHorasSemanais(Double horasSemanais) {
-        this.horasSemanais = horasSemanais;
-    }
-
-    public Double getFimDeSemana() {
-        return fimDeSemana;
-    }
-
-    public void setFimDeSemana(Double fimDeSemana) {
-        this.fimDeSemana = fimDeSemana;
-    }
-
-    public Double getBonificacao() {
-        return bonificacao;
-    }
-
-    public void setBonificacao(Double bonificacao) {
-        this.bonificacao = bonificacao;
-    }
-
-    public Integer getMetaAtingida() {
-        return metaAtingida;
-    }
-
-    public void setMetaAtingida(Integer metaAtingida) {
-        this.metaAtingida = metaAtingida;
-    }
-
-    public void pagarSalario() {
-        double valor = this.salarioBase.doubleValue() + (this.metaAtingida * this.bonificacao * this.salarioBase.doubleValue());
-        BigDecimal total = BigDecimal.valueOf(valor);
-
-        System.out.println("Salário base: R$" + String.format("%.2f", this.salarioBase.doubleValue()) + "\nValor a receber: R$ " + String.format("%.2f", total.doubleValue()));
-    }
+public interface CLT {
+    public void pagarSalario();
 }
