ae24475ded777bc7e815c030d3ee6096aed71637
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-28 17:37:39 -0300

    Refactor CLT Interface to abstract class

--- src/aula4/tt/CLT.java
+++ src/aula4/tt/CLT.java
@@ -1,5 +1,66 @@
 package aula4.tt;
 
-public interface CLT {
-    public void pagarSalario();
+import java.math.BigDecimal;
+
+public abstract class CLT {
+    private BigDecimal salarioBase;
+    private Double horasSemanais;
+    private Double fimDeSemana;
+    private Double bonificacao;
+    private Integer metaAtingida;
+
+    public CLT(BigDecimal salarioBase, Double horasSemanais, Double fimDeSemana, Double bonificacao, Integer metaAtingida) {
+        this.salarioBase = salarioBase;
+        this.horasSemanais = horasSemanais;
+        this.fimDeSemana = fimDeSemana;
+        this.bonificacao = bonificacao;
+        this.metaAtingida = metaAtingida;
+    }
+
+    public BigDecimal getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(BigDecimal salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public Double getHorasSemanais() {
+        return horasSemanais;
+    }
+
+    public void setHorasSemanais(Double horasSemanais) {
+        this.horasSemanais = horasSemanais;
+    }
+
+    public Double getFimDeSemana() {
+        return fimDeSemana;
+    }
+
+    public void setFimDeSemana(Double fimDeSemana) {
+        this.fimDeSemana = fimDeSemana;
+    }
+
+    public Double getBonificacao() {
+        return bonificacao;
+    }
+
+    public void setBonificacao(Double bonificacao) {
+        this.bonificacao = bonificacao;
+    }
+
+    public Integer getMetaAtingida() {
+        return metaAtingida;
+    }
+
+    public void setMetaAtingida(Integer metaAtingida) {
+        this.metaAtingida = metaAtingida;
+    }
+
+    public void pagarSalario() {
+        double valor = this.salarioBase.doubleValue() + (this.metaAtingida * this.bonificacao * this.salarioBase.doubleValue());
+        BigDecimal total = BigDecimal.valueOf(valor);
+
+        System.out.println("Salário base: R$" + String.format("%.2f", this.salarioBase.doubleValue()) + "\nValor a receber: R$ " + String.format("%.2f", total.doubleValue()));
+    }
 }
