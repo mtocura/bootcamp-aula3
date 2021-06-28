9c6fbd277cad6ed8dd793ff1430ed831f6f6ec84
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-28 14:14:16 -0300

    Pessoa class created

---
+++ src/aula5/Pessoa.java
@@ -0,0 +1,32 @@
+package aula5;
+
+public class Pessoa<T> implements Precedente<T>{
+    private String nome;
+    private String cpf;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public int precede(T o) {
+        return this.nome.compareToIgnoreCase((String) o);
+    }
+}
