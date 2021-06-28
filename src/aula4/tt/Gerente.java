07fc4e1ebd5894aed21682f64588003a930fb9c4
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-28 17:38:55 -0300

    Gerente class extending CLT

---
+++ src/aula4/tt/Gerente.java
@@ -0,0 +1,9 @@
+package aula4.tt;
+
+import java.math.BigDecimal;
+
+public class Gerente extends CLT{
+    public Gerente(BigDecimal salarioBase, Double horasSemanais, Double fimDeSemana, Double bonificacao, Integer metaAtingida) {
+        super(salarioBase, horasSemanais, fimDeSemana, bonificacao, metaAtingida);
+    }
+}
