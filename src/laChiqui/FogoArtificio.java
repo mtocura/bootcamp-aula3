99a0be1dee8b71cddbc597836790bab95616ccdb
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-29 11:28:28 -0300

    Criação da classe para os fogos de artificio

---
+++ src/laChiqui/FogoArtificio.java
@@ -0,0 +1,17 @@
+package laChiqui;
+
+public class FogoArtificio {
+    private String barulho;
+
+    public FogoArtificio(String barulho) {
+        this.barulho = barulho;
+    }
+
+    public String getBarulho() {
+        return barulho;
+    }
+
+    public void setBarulho(String barulho) {
+        this.barulho = barulho;
+    }
+}
