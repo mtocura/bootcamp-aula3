559ba70aa024aaadc23ebd49c881efc972c41bfe
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-29 11:25:06 -0300

    Criação da classe de convidados Meli

---
+++ src/laChiqui/Meli.java
@@ -0,0 +1,11 @@
+package laChiqui;
+
+public class Meli extends Convidado{
+    public Meli(String nome) {
+        super(nome);
+    }
+
+    public void comerBolo() {
+        System.out.println("Viva la Chiqui !!");
+    }
+}
