ab3a3ac569b56305ed6e3a68a93d3b40a4b470a2
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-29 10:27:06 -0300

    Criação da classe Moto

---
+++ src/dakar/Moto.java
@@ -0,0 +1,7 @@
+package dakar;
+
+public class Moto extends Veiculo{
+    public Moto(Double velocidade, Double aceleracao, Integer anguloDeGiro, String placa) {
+        super(velocidade, aceleracao, anguloDeGiro, placa, 300.0, 2);
+    }
+}
