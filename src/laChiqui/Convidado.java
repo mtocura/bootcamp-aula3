12c64273cbfdf29f4c247a7c1cd6a44809110298
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-06-29 11:24:49 -0300

    Criação da classe Convidado

---
+++ src/laChiqui/Convidado.java
@@ -0,0 +1,17 @@
+package laChiqui;
+
+public class Convidado {
+    private String nome;
+
+    public Convidado(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
